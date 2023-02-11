7a1418d5a436ca94b32968256c79660a0e2ddb26
yoichi-aoyama <aoyama.yoichi@gmail.com>
2023-02-11 13:40:02 +0000

    update: tetrust

---
+++ tetrust/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    for y in 0..21 {
+        for x in 0..13 {
+            if x == 0 || x == 12 || y == 20 {
+                print!("[]");
+            } else {
+                print!(" .");
+            }
+        }
+        println!();
+    }
+}
